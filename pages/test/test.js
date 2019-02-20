5b4d184acde1fc4f1e80b2339894cb4ad09df8e3
zhongtong <397580500@qq.com>
2019-02-20 10:01:35 +0800

    换行对齐算法

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,43 +1,26 @@
 const config = require("../../utils/config.js")
+const utils = require("../../utils/util.js")
 //banner
 const app = getApp()
 Page({
   data: {
     userInfo: {},
+    name: '', //姓名
+    headImgUrl: '', //头像地址
     hasUserInfo: false,
     //轮播图
     swiperCurrent: 0,
     dotsCurrent: 0,
-    addCommentsFlag:false,
+    addCommentsFlag: false, //控制评论框的显示
     myCommentValue: '',
     tempMyCommentValue: '',
-    myCommentLength: 0,   
-    slider: [{
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_1.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_2.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_3.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_4.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_5.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_6.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_7.png'
-    },
-    {
-      picUrl: config.base_path + '/www/alyp/img/sharePoster_8.png'
-    }
-    ],
-    previewImageList:[
+    myCommentLength: 0,
+    focus: false, //是否获得焦点
+    selection_start: -1, //选中起点
+
+    
+    selection_end: -1, //选中终点
+    previewImageList: [
       config.base_path + '/www/alyp/img/sharePoster_1.png',
       config.base_path + '/www/alyp/img/sharePoster_2.png',
       config.base_path + '/www/alyp/img/sharePoster_3.png',
@@ -47,19 +30,17 @@ Page({
       config.base_path + '/www/alyp/img/sharePoster_7.png',
       config.base_path + '/www/alyp/img/sharePoster_8.png'
     ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 2000,
-    duration: 1000,
-    circular: true,
-    beforeColor: "white",//指示点颜色  
-    afterColor: "coral",//当前选中的指示点颜色 
+    codeImageUrl: config.base_path + '/www/public/img/code.png',
+    onShowCanvas: false
+
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+        name: app.globalData.userInfo.nickName,
+        headImgUrl: app.globalData.userInfo.avatarUrl
       })
       console.log(app.globalData.userInfo)
     } else if (this.data.canIUse) {
@@ -68,7 +49,9 @@ Page({
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
+          name: res.userInfo.nickName,
+          headImgUrl: res.userInfo.avatarUrl
         })
       }
     } else {
@@ -78,29 +61,33 @@ Page({
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
+            name: res.userInfo.nickName,
+            headImgUrl: res.userInfo.avatarUrl
           })
         }
       })
     }
   },
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      name: e.detail.userInfo.nickName,
+      headImgUrl: e.detail.userInfo.avatarUrl
     })
   },
   //轮播图的切换事件 
-  swiperChange: function (e) {
+  swiperChange: function(e) {
     console.log(e)
     //只要把切换后当前的index传给<swiper>组件的current属性即可 
     this.setData({
       swiperCurrent: e.detail.current
     })
   },
-  dotsChange: function (e) {
+  dotsChange: function(e) {
     console.log(e)
     //只要把切换后当前的index传给<swiper>组件的current属性即可 
     this.setData({
@@ -108,7 +95,7 @@ Page({
     })
   },
   //点击指示点切换 
-  smallImgClick: function (e) {
+  smallImgClick: function(e) {
     console.log(e)
     this.setData({
       swiperCurrent: e.currentTarget.id
@@ -122,35 +109,109 @@ Page({
   //     })
   //   }
   // },
-  previewImage: function (e) {
-    let current = e.target.dataset.src
-    console.log(current)
-    console.log(this.data.slider)
-    wx.previewImage({
-      urls: this.data.previewImageList,
-      current: current
+  previewImage: function(e) {
+    // wx.showLoading({
+    //   title: '泡面就快好',
+    //   mask: true
+    // })
+    let that = this,
+      ctx = wx.createCanvasContext('sharePoster', this),
+      nameColor = '#3968B0',
+      commentColor = '#595757',
+      bgImgUrl = this.data.previewImageList[this.data.swiperCurrent],
+      windowWidth = wx.getSystemInfoSync().windowWidth,
+      windowHeight = wx.getSystemInfoSync().windowHeight,
+      info_width = wx.getSystemInfoSync().windowWidth, //底部信息区宽
+      info_height = 150, //底部信息区高
+      info_x = 0, //底部信息区x
+      info_y = 460, //底部信息区y
+      code_x = (wx.getSystemInfoSync().windowWidth / 2) - 32, //二维码信息
+      code_y = 410,
+      code_width = 64,
+      code_height = 64,
+      name_x = 40,
+      name_y = 548,
+      headImg_x = 65,
+      headImg_y = 498,
+      line_x = 110,//评论区x
+      line_y = 490,//评论区首行y
+      lineArray = utils.canvasBreakLine(that.data.myCommentValue,ctx, 160, 14)//评论多行文本换行
+    wx.getImageInfo({
+      src: bgImgUrl,
+      success(res) {
+        //画背景图
+        ctx.drawImage(res.path, 0, 0, windowWidth, windowHeight)
+        //画底部信息区白色底
+        ctx.rect(info_x, info_y, info_width, info_height)
+        ctx.setFillStyle('#fff')
+        ctx.fill()
+        //画二维码
+        ctx.drawImage(that.data.codeImageUrl,code_x,code_y,code_width,code_height)
+        //画姓名
+        ctx.setFillStyle(nameColor)
+        ctx.setFontSize(18)
+        ctx.fillText(that.data.name,name_x,name_y)
+        //画头像
+        wx.downloadFile({
+          url: that.data.headImgUrl,
+          success(res) {
+            ctx.save()
+            ctx.beginPath()
+            ctx.arc(headImg_x, headImg_y, 25, 0, 2 * Math.PI)
+            ctx.setStrokeStyle('transparent')
+            ctx.stroke()
+            ctx.clip()
+            ctx.drawImage(res.tempFilePath,40, 473, 50, 50)
+            ctx.restore()
+            lineArray.forEach(function(item,index){
+              ctx.setFillStyle(commentColor)
+              ctx.setFontSize(14)
+              ctx.fillText(item, line_x, line_y + 17 * index)
+            })
+            ctx.draw()
+          },
+          fail: function (err) {
+            console.log(err)
+          }
+        })
+        that.setData({
+          onShowCanvas: true
+        })
+      },
+      fail: function (err) {
+        console.log(err)
+      }
     })
   },
-  showAddComment: function (e) {
+  showAddComment: function(e) {
     this.setData({
-      addCommentsFlag: true
+      addCommentsFlag: true,
+      focus: true,
+      selection_start: 0,
+      selection_end: this.data.myCommentLength
     })
   },
-  cancelAddComment: function (e) {
+  cancelAddComment: function(e) {
     this.setData({
-      addCommentsFlag: false
+      addCommentsFlag: false,
+      focus: false,
+      selection_start: -1,
+      selection_end: -1
     })
   },
-  sureAddComment: function (e) {
+  sureAddComment: function(e) {
     this.setData({
       myCommentValue: this.data.tempMyCommentValue,
-      addCommentsFlag: false
+      addCommentsFlag: false,
+      focus: false,
+      selection_start: -1,
+      selection_end: -1
     })
   },
-  bindCommentInput: function (e) {
+  bindCommentInput: function(e) {
     this.setData({
       myCommentLength: e.detail.cursor,
-      tempMyCommentValue: e.detail.value.replace(/垃圾/g,'**')
+      tempMyCommentValue: e.detail.value.replace(/垃圾/g, '**')
     })
   }
 
--- utils/util.js
+++ utils/util.js
@@ -13,7 +13,24 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
+//canvas绘制多行文本换行
+function canvasBreakLine(text,ctx,wrapperWidth,fontSize){
+  let lineArray = []
+  let textareaWidth = Math.floor(wrapperWidth / fontSize)
+  while (text.length > 0) {
+    let temp = text.substr(0, textareaWidth)
+    while (ctx.measureText(temp).width < wrapperWidth && textareaWidth < text.length){
+      textareaWidth ++
+      temp = text.substr(0, textareaWidth)
+    }
+    temp = text.substr(0, textareaWidth)
+    lineArray.push(temp)
+    text = text.substr(textareaWidth)
+    textareaWidth = Math.floor(wrapperWidth / fontSize)
+  }
+  return lineArray
+}
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  canvasBreakLine: canvasBreakLine
 }
